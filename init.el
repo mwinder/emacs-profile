73ab87f81f8adb9f236f2b4881f8116c33317b36
Matthew Winder <@mwinder>
2015-05-18 21:56:29 +0100

    Added some settings.
    Added package manager config.

--- init.el
+++ init.el
@@ -1,9 +1,19 @@
 
 (custom-set-variables
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (tango-dark)))
- '(save-place t nil (saveplace))
+ '(make-backup-files nil)
+ '(show-paren-mode t)
  '(tool-bar-mode nil))
 
 (custom-set-faces
  '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+
+(global-set-key [f5] "\M-x shell")
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
