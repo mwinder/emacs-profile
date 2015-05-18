8a2989356b470d619d5124e7f454af79e0020cc8
Matthew Winder <@mwinder>
2015-05-18 08:41:37 +0100

    Initial settings

---
+++ init.el
@@ -0,0 +1,9 @@
+
+(custom-set-variables
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(save-place t nil (saveplace))
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
